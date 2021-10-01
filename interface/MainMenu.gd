60165e95bf4d198e5da15975017284a0fdf1b2ed
Guilherme Oliveira <htoguilherme@gmail.com>
2021-10-02 12:22:49 +1300

    Add menus placeholders

---
+++ interface/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+export var game_scene: PackedScene
+export var options_scene: PackedScene
+
+func _on_Start_pressed() -> void:
+	get_tree().change_scene_to(game_scene)
+
+
+func _on_Options_pressed() -> void:
+	get_tree().change_scene_to(options_scene)
+
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
