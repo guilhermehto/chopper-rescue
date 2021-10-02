d8e6f70bb90a28f3e5d1711263df13db971ee287
Guilherme Oliveira <htoguilherme@gmail.com>
2021-10-02 20:20:08 +1300

    Fix rotation and controller

--- entities/chopper/PhysicsChopper.gd
+++ entities/chopper/PhysicsChopper.gd
@@ -1,10 +1,14 @@
 extends RigidBody
 
-#const COLLECTIVE_SPEED: float = 0.3 # how quickly the collective goes up and down
-const COLLECTIVE_SPEED: float = 0.8 # how quickly the collective goes up and down
+onready var mesh: Spatial = $ChopperMesh
 
-export var engine_force: float = 6000
+const COLLECTIVE_SPEED: float = 0.8 # how quickly the collective goes up and down
+const MAX_ROTATION: float = deg2rad(30)
+
+export var engine_force: float = 10000
+export var linear_force: float = 6000
 export var tail_engine_force: float = 3000
+export var rotate_speed: float = .5
 
 var collective: float = 0 setget _set_collective
 
@@ -20,14 +24,26 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("cyclic_right") - Input.get_action_strength("cyclic_left")
 	)
 	
-	add_torque(-global_transform.basis.z * tail_engine_force / 2 * cyclic.y)
-	add_torque(global_transform.basis.x * tail_engine_force * cyclic.x)
+	add_force(global_transform.basis.z.rotated(global_transform.basis.x, 0) * linear_force * cyclic.x, Vector3.ZERO)
 	
-	var forward_force = Vector3.UP.dot(Vector3(0, rotation.x, 0))
-	add_force(global_transform.basis.z.rotated(global_transform.basis.x, 0) * engine_force * 2 * forward_force, Vector3.ZERO)
+	add_force(Vector3.RIGHT.rotated(Vector3.UP, rotation.y) * linear_force * cyclic.y, Vector3.ZERO)
 	
-	var side_force = -Vector3.UP.dot(Vector3(0, rotation.z, 0))
-	add_force(Vector3.RIGHT.rotated(Vector3.UP, rotation.y) * engine_force * 2 * side_force, Vector3.ZERO)
+	if cyclic.x != 0:
+		mesh.rotation.x = clamp(mesh.rotation.x + cyclic.x * rotate_speed * delta, -MAX_ROTATION, MAX_ROTATION)
+	elif mesh.rotation.x != 0:
+		if mesh.rotation.x > 0:
+			mesh.rotation.x = max(0, mesh.rotation.x - rotate_speed * delta)
+		else:
+			mesh.rotation.x = min(0, mesh.rotation.x + rotate_speed * delta)
+	
+	if cyclic.y != 0:
+		mesh.rotation.z = clamp(mesh.rotation.z + -cyclic.y * rotate_speed * delta, -MAX_ROTATION, MAX_ROTATION)
+	elif mesh.rotation.z != 0:
+		if mesh.rotation.z > 0:
+			mesh.rotation.z = max(0, mesh.rotation.z - rotate_speed * delta)
+		else:
+			mesh.rotation.z = min(0, mesh.rotation.z + rotate_speed * delta)
+	
 	
 
 func _set_collective(value: float) -> void:
