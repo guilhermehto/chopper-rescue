fbfc2988c71cf2adc8e51caf637b3a8f915d8e31
Guilherme Oliveira <htoguilherme@gmail.com>
2021-10-02 21:56:41 +1300

    Fix camera follow behavior

--- entities/chopper/Camera.gd
+++ entities/chopper/Camera.gd
@@ -1,19 +1,27 @@
 extends Camera
 
-var initial_rotation: Basis
-
 var initial_global_position: Vector3
 var parent_transform: Transform
 
+var target: Vector3
+
+var default_following_speed: float = 20.0
+var catch_up_following_speed: float = 25.0
+var desired_distance: float
+
 func _ready() -> void:
 	set_as_toplevel(true)
-	initial_rotation = global_transform.basis
 	initial_global_position = global_transform.origin
-	var parent_transform = get_parent().global_transform
+	desired_distance = target.distance_to(global_transform.origin)
+	desired_distance *= 1.25
 	
 	
 
 func _process(delta: float) -> void:
 	parent_transform = get_parent().global_transform
-	global_transform.origin = parent_transform.origin + initial_global_position.rotated(Vector3.UP, get_parent().rotation.y)
+	target = parent_transform.origin + initial_global_position.rotated(Vector3.UP, get_parent().rotation.y)
+	global_transform.origin.y = target.y
+	var direction = (target - global_transform.origin).normalized()
+	var speed = default_following_speed if target.distance_to(global_transform.origin) < desired_distance else catch_up_following_speed
+	global_transform.origin += direction * speed * delta
 	look_at(parent_transform.origin, Vector3.UP)
